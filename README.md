89c9cbfed0f5f95a550ba05612cf88ffddfb42c3
CHAROT Amine <charotamine@gmail.com>
2019-10-08 22:45:15 +0100

    add Azure Deployment

--- README.md
+++ README.md
@@ -18,7 +18,48 @@ The extention can also create an Azure Blueprints artifact of type `template`, `
 
 > This will generate the wanted artifact type with a default template inside.
 
+Do you want to test your blueprint without writing scripts? No problem, we have the solution :
+
+# Requirement : 
+* Azure Powershell and Az.Blueprint Module
+
+To import the blueprint : 
+* Use command ** Azure Blueprints: Import Blueprint **  
+![artifact](images/import.png)
+### Requirement : 
+* Blueprint workspace (Blueprint.json and Artifacts)
+* Management Group Id
+> This will import the blueprint to Azure. 
+
+To publish the blueprint : 
+* Use command ** Azure Blueprints: Publish Blueprint **  
+![artifact](images/publish.png)
+### Requirement : 
+* Blueprint name
+* Blueprint version
+* Management Group Id
+> This will publish the imported blueprint. 
+
+To Assign the blueprint : 
+* Use command ** Azure Blueprints: Assign Blueprint **  
+![artifact](images/assign.png)
+### Requirement : 
+* Blueprint name
+* Blueprint version
+* Management Group Id
+* Subscription Id
+* Blueprint workspace (Assign.json)
+> This will assign the publishd blueprint. 
+
+NOTE : The blueprint workspace MUST be in the root ! The blueprint name in Azure will have the SAME name as the workspace !
+
 ## Release Notes
+
+### 0.0.2
+
+* Import a blueprint to Azure
+* Publish an imported blueprint
+* Assign a published blueprint
 
 ### 0.0.1
 
--- src/extension.ts
+++ src/extension.ts
@@ -58,8 +58,106 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 	);
 
+	const importBlueprint = vscode.commands.registerCommand(
+		'extension.importBlueprint',
+		async () => {
+			try {
+				blueprintRootName = await vscode.window.showInputBox({ value: blueprintRootName, prompt: "Blueprint root folder", placeHolder: "Blueprint root folder name", password: false });
+				if (blueprintRootName == undefined || blueprintRootName.length <= 0) {
+					vscode.window.setStatusBarMessage("No root name provided");
+				}
+				else {
+					let managementGroupName = await vscode.window.showInputBox({ value: "myManagementGroup", prompt: "Management Group name", placeHolder: "Management Group name", password: false });
+					if (managementGroupName == undefined || managementGroupName.length <= 0) {
+						vscode.window.setStatusBarMessage("No management group provided");
+					}
+					else {
+						let terminal = (<any>vscode.window).createTerminal("Import blueprint");
+						terminal.show(true);
+						terminal.sendText("POWERSHELL Login-AzAccount", true);
+						terminal.sendText(`POWERSHELL Import-AzBlueprintWithArtifact -Name ${blueprintRootName} -ManagementGroupId ${managementGroupName} -InputPath ./${blueprintRootName}`, true);
+					}
+				}
+			} catch (err) {
+				if (err && err.message) {
+					vscode.window.showErrorMessage(err.message);
+				}
+			}
+		}
+	);
+
+	const publishBlueprint = vscode.commands.registerCommand(
+		'extension.publishBlueprint',
+		async () => {
+			try {
+				blueprintRootName = await vscode.window.showInputBox({ value: blueprintRootName, prompt: "Blueprint name", placeHolder: "Blueprint name", password: false });
+				if (blueprintRootName == undefined || blueprintRootName.length <= 0) {
+					vscode.window.setStatusBarMessage("No name provided");
+				}
+				else {
+					let version = await vscode.window.showInputBox({ value: "1.0.0", prompt: "Blueprint version", placeHolder: "Blueprint version", password: false });
+					if (version == undefined || version.length <= 0) {
+						vscode.window.setStatusBarMessage("No version provided");
+					}
+					else {
+						let managementGroupName = await vscode.window.showInputBox({ value: "myManagementGroup", prompt: "Management Group name", placeHolder: "Management Group name", password: false });
+						if (managementGroupName == undefined || managementGroupName.length <= 0) {
+							vscode.window.setStatusBarMessage("No management group provided");
+						}
+						else {
+							let terminal = (<any>vscode.window).createTerminal("Import blueprint");
+							terminal.show(true);
+							terminal.sendText("POWERSHELL Login-AzAccount", true);
+							terminal.sendText(`POWERSHELL $createdBlueprint = Get-AzBlueprint -ManagementGroupId ${managementGroupName} -Name ${blueprintRootName}; Publish-AzBlueprint -Blueprint $createdBlueprint -Version ${version}`, true);
+						}
+					}
+				}
+			} catch (err) {
+				if (err && err.message) {
+					vscode.window.showErrorMessage(err.message);
+				}
+			}
+		}
+	);
+
+	const assignBlueprint = vscode.commands.registerCommand(
+		'extension.assignBlueprint',
+		async () => {
+			try {
+				blueprintRootName = await vscode.window.showInputBox({ value: blueprintRootName, prompt: "Blueprint root folder", placeHolder: "Blueprint root folder name", password: false });
+				if (blueprintRootName == undefined || blueprintRootName.length <= 0) {
+					vscode.window.setStatusBarMessage("No root name provided");
+				}
+				else {
+					let subscriptionId = await vscode.window.showInputBox({ value: "xxxx-xxxx-xxxx", prompt: "Subscription Id", placeHolder: "Fill your subscription Id", password: false });
+					if (subscriptionId == undefined || subscriptionId.length <= 0) {
+						vscode.window.setStatusBarMessage("No subscription Id provided");
+					}
+					else {
+						let managementGroupName = await vscode.window.showInputBox({ value: "myManagementGroup", prompt: "Management Group name", placeHolder: "Management Group name", password: false });
+						if (managementGroupName == undefined || managementGroupName.length <= 0) {
+							vscode.window.setStatusBarMessage("No management group provided");
+						}
+						else {
+							let terminal = (<any>vscode.window).createTerminal("Import blueprint");
+							terminal.show(true);
+							terminal.sendText("POWERSHELL Login-AzAccount", true);
+							terminal.sendText(`POWERSHELL $createdBlueprint = Get-AzBlueprint -ManagementGroupId ${managementGroupName} -Name ${blueprintRootName}; New-AzBlueprintAssignment -Name ${blueprintRootName} -Blueprint $createdBlueprint -AssignmentFile ./${blueprintRootName}/Assign.json -SubscriptionId ${subscriptionId}`, true);
+						}
+					}
+				}
+			} catch (err) {
+				if (err && err.message) {
+					vscode.window.showErrorMessage(err.message);
+				}
+			}
+		}
+	);
 	context.subscriptions.push(generateBlueprint);
 	context.subscriptions.push(createTemplateArtifact);
+	context.subscriptions.push(importBlueprint);
+	context.subscriptions.push(publishBlueprint);
+
 
 }
 
